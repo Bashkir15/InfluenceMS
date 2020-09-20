79ca5fac5487edae39b9de48fda76d76c6a5985d
Forrest Collins <Forest.D.Collins@gmail.com>
2020-09-20 12:50:42 -0500

    Watch command:
    Spins up a dev-server to serve and watch the client bundle

---
+++ scripts/commands/watch.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk'
+import WebpackDevServer from 'webpack-dev-server'
+
+import { createWebpackCompiler } from '../webpack'
+
+export async function watch(projectConfig) {
+    let isFirstRun = true
+
+    console.log(chalk.cyan(`Preparing to watch files...`))
+
+    const compiler = createWebpackCompiler(projectConfig)
+    compiler.hooks.done.tap('watch-command', stats => {
+        const msg = isFirstRun
+            ? `Bundle Compiled. Watching for changes...`
+            : 'Recompiled on changes. Awaiting more...'
+        console.log(chalk.green(msg))
+        isFirstRun = false
+    })
+
+    const devServer = new WebpackDevServer(compiler, {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        host: projectConfig.host,
+        hot: true,
+        noInfo: true,
+        port: projectConfig.port,
+        publicPath: compiler.options.output.publicPath
+    })
+
+    devServer.listen(projectConfig.port, projectConfig.host, () => {
+        console.log(chalk.cyan(`Dev server listening at ${projectConfig.host}:${projectConfig.port}`))
+    })
+}
