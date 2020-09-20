f529becfae32340fa7a0024c4f1be160da4f17fa
Forrest Collins <Forest.D.Collins@gmail.com>
2020-09-20 13:17:14 -0500

    Defines the possible cli arguments

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -2,13 +2,14 @@ import commandLineArgs from 'command-line-args'
 import commandLineCommands from 'command-line-commands'
 
 import { CommandMap } from './commands'
+import { GLOBAL_CLI_ARGS } from './constants'
 import { getProjectConfig } from './get-project-config'
 
 const commandNames = Array.from(CommandMap.keys())
 
 async function cli() {
     const { argv, command: commandName } = commandLineCommands(commandNames, process.argv.slice(2))
-    const projectConfig = getProjectConfig(commandLineArgs([], { argv }), commandName)
+    const projectConfig = getProjectConfig(commandLineArgs(GLOBAL_CLI_ARGS, { argv }), commandName)
     const command = CommandMap.get(commandName)
 
     try {
---
+++ scripts/constants.ts
@@ -0,0 +1,9 @@
+export const GLOBAL_CLI_ARGS = [{
+    description: 'The target for the build. client or server',
+    name: 'buildTarget',
+    type: String
+}, {
+    description: 'Specific mode target for build',
+    name: 'mode',
+    type: String
+}]
